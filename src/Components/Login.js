f48aa8f6d609efa4d72cb65a28de51922b2c4200
joshbroawesome <joshbroawesome@gmail.com>
2021-05-19 19:37:48 -0500

    Login.js + code

---
+++ src/Components/Login.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+export default function Form() {
+
+
+    const [data, setData] = useState({
+        username: '',
+        password: ''
+    });
+
+    const getLogin = (() => {
+        axios.post(``)
+        .then(response => {
+            console.log(Object.values(response.data));
+          })
+          .catch(err => { console.log(err) });
+      })
+
+      
+      
+
+    // useEffect(() => {
+    //     console.log(data);
+    // }, [data])
+
+    const onInputChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    const onFormSubmit = () => {
+        console.log('Login Submitted and Displayed on Next Line');
+        console.log(data);
+
+    }
+
+
+    return (
+        <div className="form" >
+            <form onSubmit={(event) => { event.preventDefault(); onFormSubmit(); }}>
+                <input>
+                    <div>
+                        <label htmlFor="username"> Username </label>
+                    </div>
+                    <input
+                        id="username"
+                        type="text"
+                        name="username"
+                        placeholder="Username / Email"
+                        onChange={onInputChange}
+                    />
+                </input>
+
+                <input>
+                    <div>
+                        <label htmlFor="password"> Password </label>
+                    </div>
+                    <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password"
+                        onChange={onInputChange}
+                    />
+                    
+                        <button className="loginButton" type='submit'>Login</button>
+                    
+                </input>
+            </form>
+        </div>
+    )
+
+}
