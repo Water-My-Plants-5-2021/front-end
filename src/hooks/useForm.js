8bdce7e1ac138a22c209f2af01e3794edbd4ca7b
Barry Porch <bporch@ymail.com>
2021-05-20 11:59:10 -0400

    created useForm hook

---
+++ src/hooks/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const initialValues = {
+    username: "",
+    phoneNumber: "",
+    password: ""
+}
+
+export const useForm = e => {
+    const [data, setData] = useState(initialValues)
+
+    const onInputChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    return [data, onInputChange]
+}
