15e3e695fd54974d0b7738a5c4f37fe633d3fb17
Stephen Aranda <75341073+OniWarrior@users.noreply.github.com>
2021-05-21 23:47:11 -0500

    finished landing page. Added a navigation component, some animation, and some styling (#8)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import { Route } from "react-router-dom";
+import { Route ,Switch} from "react-router-dom";
 
-import './App.css';
+
 import Login from "./Components/Login"
 import SignIn from "./Components/SignIn"
+import Navigation from './Components/Navigation'
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <SignIn />
+      <header>
+        <Navigation/>        
+      </header>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home/>
+        </Route>
+        <Route path='/Login'>
+          <Login/>
+        </Route>
+        <Route path='/SignIn'>
+          <SignIn/>
+        </Route>
+      </Switch>
+      
+      
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+const Home = ()=>{
+
+    return(
+        <div className='container'>
+            <h2>Welcome to Water My Plants App!!</h2> 
+            <p>Ensuring that all your plants are consistently watered is actually pretty difficult.
+             Water My Plants is an app that helps to solve those problems.
+            </p>     
+            <p>
+            With an easy to use interface for creating a plant watering
+             schedule tailored to each individual plant, WaterMyPlants will 
+             remind users when it's time to feed that foliage and quench your plants' thirst.
+            </p> 
+
+            <p>
+                Sign In/ Login at the top of the page to get started!!
+            </p>     
+        </div>
+
+
+    );
+
+
+
+}
+
+export default Home;
