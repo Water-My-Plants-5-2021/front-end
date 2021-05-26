6ab9e33f9dac6f75f7f4c6efdaf0906900745d36
joshbroawesome <69223739+joshbroawesome@users.noreply.github.com>
2021-05-25 20:40:40 -0500

    Login (#13)
    * Card and
    cards with some functionality
    
    * Login edit
    
    * Delete Card.js
    
    * Delete Cards.js
    
    Co-authored-by: Barry Porch <69640436+bporch84@users.noreply.github.com>

---
+++ src/Components/EditLogin.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { useHistory } from "react-router";
+// import axios from "axios";
+
+export default function EditLogin() {
+
+    // const {  } = useHistory();
+
+    const [data, setData] = useState({
+        password: ``,
+        phoneNumber: ``
+    });
+
+    const onInputChange = event => {
+        setData({
+            ...data,
+            [event.target.name]: event.target.value
+        })
+    };
+
+    // const onFormSubmit = e => {
+    //     e.preventDefault();
+        
+    // }
+
+    return (
+        <div className="formeditlogin">
+            <form onSubmit={onFormSubmit}>
+                <div>
+                    <label><h2>Edit</h2></label>
+                </div>
+                
+                <div>
+                    <label htmlFor="password"> New Password </label>
+                </div>
+                <input
+                    id="password"
+                    type="text"
+                    name="password"
+                    placeholder="Password"
+                    onChange={onInputChange}
+                />
+
+                <div>
+                    <label htmlFor="phoneNumber"> New Phone Number </label>
+                </div>
+                <input
+                    id="phoneNumber"
+                    type="text"
+                    name="phoneNumber"
+                    placeholder="Phone Number"
+                    onChange={onInputChange}
+                />
+                
+                    <button className="editLoginButton" type='submit'>Sign-Up</button>           
+            </form>
+        </div>
+    )}
